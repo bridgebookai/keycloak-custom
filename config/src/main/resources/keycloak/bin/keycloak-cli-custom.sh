adaafd0a88e5c5611c9dd17ca6296cb161da69e2
lbusch-bridgebrook <lbusch@bridgebrookai.com>
2025-12-30 12:45:12 -0700

    feat: add bridgebook configuration

--- config/src/main/resources/keycloak/bin/keycloak-cli-custom.sh
+++ config/src/main/resources/keycloak/bin/keycloak-cli-custom.sh
@@ -6,6 +6,32 @@ echo ""
 echo "----------------- keycloak_custom_config.sh ---------------"
 echo ""
 
-createRealm example2
-createClient example2 client1
-createUser example2 user1
+echo ""
+echo "--- Assigning service account roles ---"
+echo ""
+
+# Assign manage-users role to postman-client service account
+echo "Assigning manage-users role to postman-client service account..."
+${KCADM} add-roles -r bridgebook \
+  --uusername service-account-postman-client \
+  --cclientid realm-management \
+  --rolename manage-users \
+  ${KCADM_CONFIG}
+
+# Assign manage-users role to core-service-api service account  
+echo "Assigning manage-users role to core-service-api service account..."
+${KCADM} add-roles -r bridgebook \
+  --uusername service-account-core-service-api \
+  --cclientid realm-management \
+  --rolename manage-users \
+  ${KCADM_CONFIG}
+
+echo "Service account role assignments completed."
+
+echo ""
+echo "--- Creating test Users ---"
+echo ""
+
+echo "Creating test users for the bridgebook realm"
+
+
